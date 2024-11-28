2252aeeb9ee780baacb1f72e5063a3f28971e61c
fufaich <fufaich@gmail.com>
2024-11-28 10:41:37 +0300

    Добавлена проверка роли админа, для отображения админских кнопок

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -1,16 +1,34 @@
 <script setup>
-
 import ButtonComponent from '@/components/ButtonComponent.vue'
+import { onMounted, ref } from 'vue'
+
+const isAdmin = ref(false);
+
+const checkRole = async () => {
+  const role = localStorage.getItem('role');
+  isAdmin.value = (role === "admin");
+
+}
+
+
+
+
+onMounted(() => {
+  checkRole();
+});
 </script>
 
 <template>
   <div class="flex flex-col items-center h-screen gap-4">
-    <ButtonComponent label="Кнопка 1" class="space-y-4 mt-4" path="/test"/>
-    <ButtonComponent label="Кнопка 2" class="space-y-4" path="/test"/>
-    <ButtonComponent label="Кнопка 2" class="space-y-4" path="/test"/>
-    <ButtonComponent label="Кнопка 2" class="space-y-4" path="/test"/>
-    <ButtonComponent label="Кнопка 2" class="space-y-4" path="/test"/>
-    <ButtonComponent label="Кнопка 2" class="space-y-4" path="/test"/>
+    <ButtonComponent label="Задачи" class="space-y-4 mt-4" path="/test"/>
+
+    <ButtonComponent v-if="isAdmin" label="Пользователи" class="space-y-4" path="/test"/>
+    <ButtonComponent v-if="isAdmin" label="Выполненные задачи" class="space-y-4" path="/test"/>
+
+    <ButtonComponent label="Эксперименты" class="space-y-4" path="/test"/>
+    <ButtonComponent label="Миссии" class="space-y-4" path="/test"/>
+    <ButtonComponent label="Ресурсы" class="space-y-4" path="/test"/>
+    <ButtonComponent label="Отчёты" class="space-y-4" path="/test"/>
 
   </div>
 </template>
