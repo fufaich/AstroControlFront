fdb2e056d8b427dcf222dd0ac18dd08052a4554b
fufaich <fufaich@gmail.com>
2024-12-01 21:18:49 +0300

    Refactor

--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -1,7 +1,5 @@
 <script setup>
   import { useRoute } from 'vue-router';
-  import axios from 'axios'
-  import { onMounted} from 'vue'
   import TableComponent from '@/components/TableComponent.vue'
   const route = useRoute();
   const tableName = route.params.tableName
@@ -11,7 +9,6 @@
 
 <template>
 <div class="flex-col">
-  <h2 class="font-normal text-4xl">Таблица: {{tableName}}</h2>
   <TableComponent :table_name="tableName"></TableComponent>
 </div>
 </template>
